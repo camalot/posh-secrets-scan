9f0faaff395814034dfe2429b585f0efc6db524f
camalot <camalot@gmail.com>
2018-01-26 23:25:07 -0600

    try to run tests

--- .deploy/test.ps1
+++ .deploy/test.ps1
@@ -1,3 +1,39 @@
 #!/usr/bin/env pwsh
 
-Write-Host "Running Tests...";
+if ($PSCommandPath -eq $null) {
+    $CommandRootPath = (Split-Path -Parent $MyInvocation.MyCommand.Path);
+}
+else {
+    $CommandRootPath = (Split-Path -Parent $PSCommandPath);
+}
+
+$isCI = $ENV:CI -cmatch '[Tt]rue';
+
+if (-not (Get-Module -ListAvailable -Name "pester")) {
+    Install-Module -Name "pester";
+}
+
+Import-Module "pester" -Verbose -Force;
+$cdir = $PWD;
+
+
+$testsDir = (Join-Path -Path "$CommandRootPath" -ChildPath "..\tests" -Resolve);
+$scriptDir = (Join-Path -Path "$CommandRootPath" -ChildPath "..\" -Resolve);
+
+$outDir = (Join-Path -Path "$CommandRootPath" -ChildPath "..\bin\");
+
+if ( !(Test-Path -Path $outDir) ) {
+    New-Item -ItemType "directory" -Path $outDir | Out-Null;
+}
+
+Set-Location -Path $testsDir | Out-Null;
+
+$psModuleFiles = "$scriptDir\*.ps*1";
+
+$tests = (Get-ChildItem -Path "$testsDir\*.Tests.ps1" | % { $_.FullName });
+$coverageFiles = (Get-ChildItem -Path "$psModuleFiles") | where { $_.Name -inotmatch "\.tests\.ps1$" -and $_.Name -inotmatch "\.psd1$" } | % { $_.FullName };
+$resultsOutput = (Join-Path -Path $outDir -ChildPath "secrets-scan.results.xml");
+
+Invoke-Pester -Script $tests -OutputFormat NUnitXml -OutputFile $resultsOutput -CodeCoverage $coverageFiles -Strict -EnableExit:$isCI;
+
+Set-Location -Path $cdir | Out-Null;
